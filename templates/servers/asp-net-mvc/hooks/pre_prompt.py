37d2c2c27f3cb19fe7dafa05ae67469f74a78124
Paul Fryer <pfryer-git@amazon.com>
2024-09-24 19:08:29 +0800

    working on cookiecutter template.

--- templates/servers/asp-net-mvc/hooks/pre_prompt.py
+++ templates/servers/asp-net-mvc/hooks/pre_prompt.py
@@ -8,7 +8,7 @@ def is_docker_installed() -> bool:
     except Exception:
         return False
 
-if __name__ == "__main__":
-    if not is_docker_installed():
-        print("ERROR: Docker is not installed.")
-        sys.exit(1)
+#if __name__ == "__main__":
+    #if not is_docker_installed():
+    #    print("ERROR: Docker is not installed.")
+    #    sys.exit(1)
