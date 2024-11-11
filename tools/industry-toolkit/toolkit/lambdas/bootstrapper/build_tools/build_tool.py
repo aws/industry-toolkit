0e34604766a4448186d929f97eeb9f1a69b86ca2
Greg Chagnon <gchagnon@amazon.com>
2024-11-11 11:51:46 -0500

    Add dependency

--- tools/industry-toolkit/toolkit/lambdas/bootstrapper/build_tools/build_tool.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BuildTool(ABC):
-    @abstractmethod
-    def write_config_files(self, project_dir: str):
-        """Configure the build tool to publish an image to ECR."""
-        pass
--- tools/industry-toolkit/toolkit/lambdas/bootstrapper/codegen/codegen.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Codegen(ABC):
-    @abstractmethod
-    def generate_project(self, project_id: str, event: dict):
-        """Generates a project based on the provided event data."""
-        pass
